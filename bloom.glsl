d632a4b74bb58051a929cc403b0311d131e37252
zhacgsn <1098519436@qq.com>
2026-02-17 16:33:07 +0800

    Trim shader set and simplify README config

--- bloom.glsl
+++ bloom.glsl
@@ -37,14 +37,14 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
   vec4 color = texture(iChannel0, uv);
 
-  vec2 step = vec2(1.414) / iResolution.xy;
+  vec2 step = vec2(1.1) / iResolution.xy;
 
   for (int i = 0; i < 24; i++) {
     vec3 s = samples[i];
     vec4 c = texture(iChannel0, uv + s.xy * step);
     float l = lum(c);
-    if (l > 0.2) {
-      color += l * s.z * c * 0.2;
+    if (l > 0.35) {
+      color += l * s.z * c * 0.08;
     }
   }
 
